c7a0b29330b6228d6c45b5051a0f1413a6859019
emeery <gerardoemery@live.com.mx>
2018-03-29 13:26:43 -0600

    test spies

--- src/test/componentes/GastosForm.test.js
+++ src/test/componentes/GastosForm.test.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import {shallow} from 'enzyme';
+import moment from 'moment';
 import GastosForm 
 from '../../componentes/GastosForm';
 import gastos from '../accesorios/gastos';
+
 test('componente GastosForm',()=>{
     const envoltura = shallow(<GastosForm/>);
     expect(envoltura).toMatchSnapshot();
@@ -48,7 +50,7 @@ test('cambio de monto montoCambio',()=>{
     expect(envoltura.state('monto')).toBe(value);
 }); 
 
-test('cambio de monto montoCambio',()=>{
+test('cambio de monto montoCambio invalido',()=>{
     const value = '12.122';
     const envoltura = shallow(<GastosForm/>);
     envoltura.find('input').at(1).simulate('change',{
@@ -56,3 +58,34 @@ test('cambio de monto montoCambio',()=>{
     });
     expect(envoltura.state('monto')).toBe('');
 }); 
+// sin haber sido llamado
+test('onSubmit GastosForm',()=>{
+    const onSubmitSpy = jest.fn();
+    const envoltura = shallow(<GastosForm 
+        gasto={gastos[0]} onSubmit={onSubmitSpy} /> );
+    envoltura.find('form').simulate('submit', {
+        preventDefault: () => {}
+    });
+    
+    expect(envoltura.state('error')).toBe('')
+    expect(onSubmitSpy).toHaveBeenLastCalledWith({
+            descripcion: gastos[0].descripcion,
+            monto: gastos[0].monto,
+            nota: gastos[0].nota,
+            date: gastos[0].date
+    });
+});
+
+test('establece la fecha onDatesChange DtaPcker',()=>{
+    const ahora = moment();
+    const envoltura = shallow(<GastosForm/>);
+    envoltura.find('SingleDatePicker').prop('onDateChange')(ahora);
+    expect(envoltura.state('date')).toEqual(ahora)
+});
+
+test('establece onFocusChange DtaPcker',()=>{
+    const focused= true;
+    const envoltura = shallow(<GastosForm/> );
+    envoltura.find('SingleDatePicker').prop('onFocusChange')({ focused });
+    expect(envoltura.state('calendarFocused')).toBe(focused);
+});
