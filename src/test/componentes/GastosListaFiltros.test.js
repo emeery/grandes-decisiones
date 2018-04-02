5829680e6e06e3abe66491e717f5cbad9233cdbc
emeery <gerardoemery@live.com.mx>
2018-04-02 15:40:59 -0500

    cap test

--- src/test/componentes/GastosListaFiltros.test.js
+++ src/test/componentes/GastosListaFiltros.test.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {shallow} from 'enzyme';
+import moment from 'moment';
 import {GastosListaFiltros} from '../../componentes/GastosListaFiltros';
 import {filtros, altfiltros} from '../accesorios/filtros'
 
@@ -35,11 +36,45 @@ test('snapshot GastosListaFiltros con altfiltros',()=>{
     expect(envoltura).toMatchSnapshot();
 });
 
-// beforeEach(()=>{
-//     crearGastito = jest.fn();
-//     history = { push: jest.fn() };
-//     envoltura = shallow(<PaginaSumaGasto
-//         history={history}
-//         crearGasto={crearGastito}      
-//         />);
-// });
+test('controla el cambio de texto',()=>{
+    const value = 'renta';
+    envoltura.find('input').simulate('change', {
+        target: { value }
+    });
+    expect(setTextFilters).toHaveBeenLastCalledWith(value);
+});
+
+test('deberia ordenar por fecha',()=>{
+    const value = 'date';
+    envoltura.setProps({
+        filtros: altfiltros
+    });
+    envoltura.find('select').simulate('change',{
+        target: { value }
+    });
+    expect(sortByDate).toHaveBeenCalled();
+});
+
+test('deberia ordenar por monto',()=>{
+   const value = 'monto';
+   envoltura.find('select').simulate('change',{
+       target: { value }
+   });
+   expect(sortByAmount).toHaveBeenCalled();
+});
+
+test('controla el cambio de fecha', ()=>{
+    const startDate = moment(0).add(4, 'years');
+    const endDate = moment(0).add(8, 'years');
+    envoltura.find('DateRangePicker').prop('onDatesChange')({startDate, endDate }); 
+    expect(setStartDate).toHaveBeenLastCalledWith(startDate);
+    expect(setEndDate).toHaveBeenLastCalledWith(endDate);
+});
+
+test('deberia controlar cambio enfoque', ()=>{
+    const calendarFocused = 'endDate';
+    envoltura.find('DateRangePicker').prop('onFocusChange')(calendarFocused);
+    expect(envoltura.state('calendarFocused')).toBe(calendarFocused);
+});
+
+
