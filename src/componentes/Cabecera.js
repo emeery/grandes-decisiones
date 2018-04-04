70b0aecf7ee7bfb4864eab75e56ccd67426926f1
emeery <gerardoemery@live.com.mx>
2018-04-03 23:48:28 -0500

    cabecera estrategias

--- src/componentes/Cabecera.js
+++ src/componentes/Cabecera.js
@@ -4,8 +4,8 @@ import {NavLink} from 'react-router-dom';
 // <NavLink to="/ayuda" activeClassName='is-active'>Ayuda</NavLink>
 const Cabecera = () => (
     <header>
-    <h1>Libros de Notas.</h1>
-    <h3>La pieza faltante.</h3>
+    <h1>Cuales son tus gastos ?</h1>
+    <h3>estrategias para el ahorro</h3>
     <NavLink to="/" activeClassName='is-active' exact={true} >Escritorio</NavLink>
     <NavLink to="/crealo" activeClassName='is-active'>Crear Gastos</NavLink>
     
